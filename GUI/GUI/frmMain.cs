289461502374df4d7b021f0eb66dc6131463d5af
Alebardock <Alejandrod.morales@hotmail.com>
2023-09-11 19:33:00 -0300

    Mejorar exepciones

--- GUI/GUI/frmMain.cs
+++ GUI/GUI/frmMain.cs
@@ -15,22 +15,20 @@ using Managers;
 namespace GUI
 {
     public partial class frmMain : Form
-    { 
-       
-
-        private List<Item> listItem;
+    {
+        private List<Item> listArticle;
         private int currentIndex = 0;
-        List<string> url;
+        private List<string> url;
         public frmMain()
         {
             InitializeComponent();
-        }
 
+
+        }
         private void bExit_Click(object sender, EventArgs e)
         {
             Close();
         }
-
         public void showBottons(bool show)
         {
             bAdd.Visible = show;
@@ -38,7 +36,6 @@ namespace GUI
             bDelete.Visible = show;
             tituleUser.Text = "Maxi Programa";
         }
-
         private void bAdd_Click(object sender, EventArgs e)
         {
             frmAdd windowAdd = new frmAdd();
@@ -51,40 +48,30 @@ namespace GUI
             windowEdit.ShowDialog();
 
         }
-
-
-        private void bFilter_Click(object sender, EventArgs e)
-        {
-            ItemManager iManager = new ItemManager();
-            dgArticles.DataSource = iManager.FiltbyName(tbSearch.Text);
-          
-        }
-
-
         private void tbSearch_TextChanged(object sender, EventArgs e)
         {
             ItemManager iManager = new ItemManager();
             dgArticles.DataSource = iManager.FiltbyName(tbSearch.Text);
-           
-        }
+            if (dgArticles.CurrentRow == null)
+            {
+                pbImgArticles.Load("https://previews.123rf.com/images/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-sin-foto-o-icono-de-imagen-en-blanco-cargando-im%C3%A1genes-o-marca-de-imagen-faltante-imagen-no.jpg");
 
+            }
+
+        }
         private void frmMain_Load_1(object sender, EventArgs e)
         {
-            loadGrid();
-        }
 
+            ItemManager iManager = new ItemManager();
+            dgArticles.DataSource = iManager.Listacompleta();
+        }
         private void loadGrid()
         {
             ItemManager iManager = new ItemManager();
             try
-
             {
-                listItem = iManager.Listacompleta();
-                dgArticles.DataSource = listItem;
-                dgArticles.Columns["Id"].Visible = false;
-                dgArticles.Columns["Description"].Visible = false;
-               // rtbDescription.Text = listItem.Description;
-
+                listArticle = iManager.Listacompleta();
+                dgArticles.DataSource = listArticle;
 
             }
             catch (Exception ex)
@@ -92,7 +79,6 @@ namespace GUI
                 MessageBox.Show(ex.ToString());
             }
         }
-
         private void bDelete_Click(object sender, EventArgs e)
         {
             ItemManager iManager = new ItemManager();
@@ -115,28 +101,47 @@ namespace GUI
         }
         private void dgArticles_SelectionChanged(object sender, EventArgs e)
         {
-            
+            DataGridViewRow currentSelectedRow = dgArticles.CurrentRow;
             ItemManager iManager = new ItemManager();
             Item selected = (Item)dgArticles.CurrentRow.DataBoundItem;
             url = iManager.imagesOfItems(selected.ID());
 
+            if (currentSelectedRow != null)
+            {
+                // La selección cambia de una fila a otra y cambio el currentIndex a cero para que comienze con la primer foto de la nueva selección.
+                currentIndex = 0;
+                LoadImageAtIndex(currentIndex);
+            }
+        }
+        //divido la parte que se encarga de cargar la imagen segun un indice.
+        private void LoadImageAtIndex(int index)
+        {
             try
             {
-                if (url.Count() != 0 && currentIndex < url.Count())
+                if (url.Count() != 0 && index >= 0 && index < url.Count())
                 {
                     // Carga la URL en función del índice actual
-                    if (selected.Description != "")
-                        rtbDescription.Text = selected.Description; 
-                    if(url[currentIndex] != "")
-                        pbImgArticles.Load(url[currentIndex]);
-
+                    pbImgArticles.Load(url[index]);
                 }
             }
             catch (System.Net.WebException ex)
             {
-                pbImgArticles.Load("https://lh3.googleusercontent.com/drive-viewer/AITFw-wS6RAUNTNl47sUUVoPu5qMvbp08NQ48aWAXQUFn-TsDK8497WjmJavnGyi0sS0Uvknmg17fx6wTY7MQYhYDIIRn551=w1366-h618");
-                //MessageBox.Show("La Ruta : " + url[currentIndex] + " es inaccesible");
-                //"https://w7.pngwing.com/pngs/22/842/png-transparent-picture-frame-blue-border-empty-blank-isolated-thumbnail.png"
+
+                if (ex.ToString().Contains("404"))
+                {
+                    pbImgArticles.Load("https://www.antevenio.com/wp-content/uploads/2017/08/15-ejemplos-de-paginas-404-que-generan-engagement-1.jpg");
+                    MessageBox.Show(ex.ToString());
+                }
+                else if (ex.ToString().Contains("403"))
+                {
+                    pbImgArticles.Load("https://i.insider.com/605a704f106eb50019d053e0?width=1200&format=jpeg");
+                    MessageBox.Show(ex.ToString());
+                }
+                else
+                {
+                    pbImgArticles.Load("https://previews.123rf.com/images/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-sin-foto-o-icono-de-imagen-en-blanco-cargando-im%C3%A1genes-o-marca-de-imagen-faltante-imagen-no.jpg");
+                    MessageBox.Show(ex.ToString() + " La Ruta : " + url[index] + " es inaccesible");
+                }
             }
         }
 
@@ -145,21 +150,20 @@ namespace GUI
             if (currentIndex > 0)
             {
                 currentIndex--;
-                // Vuelve a cargar la URL retrocedida
-                dgArticles_SelectionChanged(sender, e);
+                LoadImageAtIndex(currentIndex);
             }
         }
-
         private void btnAvanzar_Click(object sender, EventArgs e)
         {
             if (currentIndex < url.Count - 1)
             {
                 currentIndex++;
-                // Vuelve a cargar la URL avanzada
-                dgArticles_SelectionChanged(sender, e);
+                LoadImageAtIndex(currentIndex);
             }
 
         }
+
+
     }
 
 }
