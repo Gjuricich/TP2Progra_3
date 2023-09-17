fb1cdcafafd207fcfc3c0bcce25af51f6a6bc33c
Gjuricich <132496477+Gjuricich@users.noreply.github.com>
2023-09-17 11:18:06 -0300

    Métodos Add, upgrade, delete en Brand Manager

--- GUI/Managers/BrandManager.cs
+++ GUI/Managers/BrandManager.cs
@@ -41,5 +41,83 @@ namespace Managers
                 dataManager.closeConection();
             }
         }
+
+        public void deleteBrand(int ID)
+        {
+
+            DataManager data = new DataManager();
+            try
+            {
+
+                data.setQuery("delete from MARCAS where Id=@ID ");
+                data.setParameter("@ID", ID);
+                data.executeRead();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                data.closeConection();
+            }
+        }
+
+        public void addBrand(List<Brand> brand)
+        {
+            DataManager dataManager = new DataManager();
+            try
+            {
+
+
+                for (int i = 0; i < brand.Count(); i++)
+                {
+
+                    dataManager = new DataManager();
+                    dataManager.setQuery("INSERT INTO MARCAS (descripcion) values (@Description)");
+                    dataManager.setParameter("@Description", brand[i].Descripcion);
+                    dataManager.executeRead();
+                    dataManager.closeConection();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dataManager.closeConection();
+            }
+
+        }
+
+
+        public void updateBrand(List<Brand> brand)
+        {
+            DataManager dataManager = new DataManager();
+            try
+            {
+
+                for (int i = 0; i < brand.Count(); i++)
+                {
+                    dataManager = new DataManager();
+                    dataManager.setQuery("UPDATE MARCAS set descripcion=@Description  WHERE Id = @Id");
+                    dataManager.setParameter("@Description", brand[i].Descripcion);
+                    dataManager.setParameter("@Id", brand[i].Id);
+                    dataManager.executeRead();
+                    dataManager.closeConection();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dataManager.closeConection();
+            }
+
+        }
     }
 }
